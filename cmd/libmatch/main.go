9ec4d7b12154c88f1fa9008a03ba26ccaafceadf
abhchand <me@abhchand.me>
2022-01-18 23:24:35 -0500

    Using custom templates for app and command help text

--- cmd/libmatch/main.go
+++ cmd/libmatch/main.go
@@ -27,6 +27,31 @@ func main() {
 		&commands.VersionCommand,
 	}
 
+	cli.AppHelpTemplate = `
+Usage: {{.HelpName}} [GLOBAL OPTIONS] COMMAND [OPTIONS]
+
+{{.Usage}}
+https://github.com/abhchand/libmatch#README
+
+{{if .Commands}}
+COMMANDS:
+{{range .Commands}}{{if not .HideHelp}}   {{join .Names ", "}}{{ "\t"}}{{.Usage}}{{ "\n" }}{{end}}{{end}}{{end}}{{if .VisibleFlags}}
+GLOBAL OPTIONS:
+   {{range .VisibleFlags}}{{.}}
+   {{end}}{{end}}
+
+Run 'libmatch COMMAND --help' for more information on a command.
+	`
+
+	cli.CommandHelpTemplate = `Usage: {{.HelpName}} [GLOBAL OPTIONS] COMMAND [OPTIONS]
+
+{{.Usage}}
+{{if .VisibleFlags}}
+OPTIONS:
+   {{range .VisibleFlags}}{{.}}
+   {{end}}{{end}}
+`
+
 	if err := app.Run(os.Args); err != nil {
 		// log.Error(err)
 		fmt.Println(err)
