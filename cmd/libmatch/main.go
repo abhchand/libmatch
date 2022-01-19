097382162648b627655bf23191e048398f32a7b4
abhchand <me@abhchand.me>
2022-01-18 23:26:04 -0500

    Update `Usage` and `Description`

--- cmd/libmatch/main.go
+++ cmd/libmatch/main.go
@@ -16,8 +16,8 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "libmatch"
 	app.HelpName = filepath.Base(os.Args[0])
-	app.Usage = "A Go library for solving matching problems"
-	app.Description = "For documentation, visit https://github.com/abhchand/libmatch"
+	app.Usage = "A library for solving matching problems"
+	app.Description = "For documentation, visit https://github.com/abhchand/libmatch#README"
 	app.Version = version.Version()
 	app.EnableBashCompletion = true
 	app.Flags = config.GlobalFlags
@@ -53,7 +53,6 @@ OPTIONS:
 `
 
 	if err := app.Run(os.Args); err != nil {
-		// log.Error(err)
 		fmt.Println(err)
 	}
 
