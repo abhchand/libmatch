1e871187c424119ab731796dc6d20d117420df57
abhchand <me@abhchand.me>
2022-01-28 15:00:39 -0500

    Add Phase 3 of SRP: Cyclical Elimination

---
+++ pkg/algo/srp/cyclical_elimination.go
@@ -0,0 +1,91 @@
+package srp
+
+import (
+	"github.com/abhchand/libmatch/pkg/core"
+)
+
+type cyclePair struct {
+	x, y *core.Member
+}
+
+func phase3CyclicalElimnation(pt *core.PreferenceTable) core.MatchResult {
+	res := phase3CyclicalElimnationWithSeed(pt, "")
+	return res
+}
+
+func phase3CyclicalElimnationWithSeed(pt *core.PreferenceTable, seed string) core.MatchResult {
+	var startingMember *core.Member
+	var loopIdx int
+	res := core.MatchResult{}
+
+	for true {
+		if loopIdx == 0 && seed != "" {
+			startingMember = (*pt)[seed]
+		} else {
+			// Find the first memeber with at least two preferences
+			for _, member := range *pt {
+				if len(member.PreferenceList().Members()) >= 2 {
+					startingMember = member
+					break
+				}
+			}
+		}
+
+		pairs := detectCycle(pt, startingMember)
+		eliminateCycle(pt, pairs)
+
+		if !pt.IsStable() {
+			return res
+		}
+
+		if pt.IsComplete() {
+			break
+		}
+
+		loopIdx++
+	}
+
+	res.Mapping = make(map[string]string)
+	for name, member := range *pt {
+		res.Mapping[name] = member.PreferenceList().Members()[0].Name()
+	}
+
+	return res
+}
+
+func detectCycle(pt *core.PreferenceTable, startingMember *core.Member) []cyclePair {
+	pairs := []cyclePair{
+		{x: startingMember},
+	}
+
+	lastSeenAt := make(map[string]int, 0)
+	lastSeenAt[startingMember.Name()] = 1
+	currentMemberIdx := 0
+
+	for true {
+		currentMember := pairs[currentMemberIdx].x
+
+		newPair := cyclePair{
+			x: currentMember.SecondPreference().LastPreference(),
+			y: currentMember.SecondPreference(),
+		}
+
+		pairs = append(pairs, newPair)
+
+		if idx := lastSeenAt[newPair.x.Name()]; idx > 0 {
+			pairs = pairs[idx:]
+			break
+		}
+
+		lastSeenAt[newPair.x.Name()] = currentMemberIdx + 1
+		currentMemberIdx = currentMemberIdx + 1
+	}
+
+	return pairs
+}
+
+func eliminateCycle(pt *core.PreferenceTable, pairs []cyclePair) {
+	for p := range pairs {
+		(pairs[p].x).Reject(pairs[p].y)
+	}
+}
---
+++ pkg/algo/srp/cyclical_elimination_test.go
@@ -0,0 +1,46 @@
+package srp
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/abhchand/libmatch/pkg/core"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPhase3CyclicalElimnation(t *testing.T) {
+	wanted := core.MatchResult{
+		Mapping: map[string]string{
+			"A": "F",
+			"B": "E",
+			"C": "D",
+			"D": "C",
+			"E": "B",
+			"F": "A",
+		},
+	}
+
+	testCases := []string{"A", "B", "C", "D", "E", "F", ""}
+
+	for tc := range testCases {
+		title := fmt.Sprintf("With seed: %v", testCases[tc])
+
+		t.Run(title, func(t *testing.T) {
+			pt := core.NewPreferenceTable(&[]core.MatchEntry{
+				{Name: "A", Preferences: []string{"B", "F"}},
+				{Name: "B", Preferences: []string{"E", "F", "A"}},
+				{Name: "C", Preferences: []string{"D", "E"}},
+				{Name: "D", Preferences: []string{"F", "C"}},
+				{Name: "E", Preferences: []string{"C", "B"}},
+				{Name: "F", Preferences: []string{"A", "B", "D"}},
+			})
+
+			fmt.Printf("=== RUNNING WITH SEED: %v\n", testCases[tc])
+
+			result := phase3CyclicalElimnationWithSeed(&pt, testCases[tc])
+
+			assert.True(t, reflect.DeepEqual(wanted, result))
+		})
+	}
+}
