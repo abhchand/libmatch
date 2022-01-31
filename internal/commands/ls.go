604b656271a7e41378f3a45dd89421bbe144d4a0
abhchand <me@abhchand.me>
2022-01-31 17:26:04 -0500

    Crediting my boy Irving

--- internal/commands/ls.go
+++ internal/commands/ls.go
@@ -13,6 +13,7 @@ var SEQ_RESET = "\033[0m"
 var AlgorithmDescriptions = map[string]string{
 	"SRP": `Stable Roommates Problem
 Find a stable matching within an even-sized set.
+Implements Irving's (1985) algorithm.
 https://en.wikipedia.org/wiki/Stable_roommates_problem.
 `,
 }
