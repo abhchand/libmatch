3df4ef2d2036c685741671baf43ec87251a8d2ee
abhchand <me@abhchand.me>
2022-02-06 00:11:16 -0500

    `v0.1.0-beta.1`

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -6,7 +6,7 @@ import (
 
 // The build script parses the version from this line.
 // Check the regex in `build.sh` before modifying this!
-var version = "0.0.1"
+var version = "0.1.0-beta.1"
 
 func Formatted() string {
 	return fmt.Sprintf("v%v", version)
